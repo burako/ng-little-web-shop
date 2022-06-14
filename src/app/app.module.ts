e199850147f9055acf49683906ba03eec0eddae0
burako <burakokumus3@gmail.com>
2022-06-14 23:51:59 +0100

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductListItemComponent } from './product-list-item/product-list-item.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { CartComponent } from './cart/cart.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductListComponent,
+    ProductListItemComponent,
+    ProductDetailComponent,
+    CartComponent,
+    ConfirmationComponent
   ],
   imports: [
     BrowserModule,
